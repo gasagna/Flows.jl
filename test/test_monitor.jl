521d56886dca7d0d848364645d8f77fafbf6c3cd
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-05-09 21:14:01 +0100

    tested support for multiple monitors

--- test/test_monitor.jl
+++ test/test_monitor.jl
@@ -1,10 +1,8 @@
 using Base.Test
 using IMEXRKCB
 
-@testset "monitor      " begin
-
-    f1(x) = x[1]
-    m = Monitor([1.0], f1)
+@testset "one monitor  " begin
+    m = Monitor([1.0], x->x[1])
     
     push!(m, [0.0], 0.0)
     push!(m, [1.0], 0.1)
@@ -13,4 +11,20 @@ using IMEXRKCB
 
     @test m.times   == [0.0, 0.1, 0.2, 0.3]
     @test m.samples == [0.0, 1.0, 2.0, 3.0]
+end
+
+
+@testset "two monitors " begin
+    m1 = Monitor([1.0], x->x[1]  )
+    m2 = Monitor([1.0], x->x[1]^2)
+    
+    push!((m1, m2), [0.0], 0.0)
+    push!((m1, m2), [1.0], 0.1)
+    push!((m1, m2), [2.0], 0.2)
+    push!((m1, m2), [3.0], 0.3)
+
+    @test m1.times   == [0.0, 0.1, 0.2, 0.3]
+    @test m2.times   == [0.0, 0.1, 0.2, 0.3]
+    @test m1.samples == [0.0, 1.0, 2.0, 3.0]
+    @test m2.samples == [0.0, 1.0, 4.0, 9.0]
 end
