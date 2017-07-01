cc1428d729a3a678f5db48330ecee91d5831584f
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-07-01 17:12:57 +0100

    changed variable name

--- src/quadrature.jl
+++ src/quadrature.jl
@@ -1,20 +1,20 @@
 # This defines a type that wraps the main state vector `x::X` augmented
-# with an appropriate object `y::Y` used for pure quadrature integration.
+# with an appropriate object `q::Q` used for pure quadrature integration.
 # Note that the type is immutable, hence if a scalar function need to be 
 # integrated, its value is stored in a one-element vector
-struct AugmentedState{X, Y}
+struct AugmentedState{X, Q}
     x::X
-    y::Y
+    q::Q
 end
 
-aug_state(x, y) = AugmentedState(x, y)
+aug_state(x, q) = AugmentedState(x, q)
 aug_state(x)    = x
 
 # extract state part from an augmented state
 _state(x::AugmentedState) = x.x
 _state(x) = x
-_quad(x::AugmentedState) = x.y
-_state_quad(x::AugmentedState) = (x.x, x.y)
+_quad(x::AugmentedState) = x.q
+_state_quad(x::AugmentedState) = (x.x, x.q)
 _state_quad(x) = (x, )
 
 # Used in the definition of a scheme for proper allocation of storage. Note
