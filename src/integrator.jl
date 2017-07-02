b05fdfff1dd9dd10e1ac8e96da9db6c1690e0082
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-07-03 00:04:09 +0100

    fixed callable interface

--- src/integrator.jl
+++ src/integrator.jl
@@ -48,8 +48,10 @@ integrator(g, A, q, scheme::IMEXRKScheme, Δt::Real) =
 (I::Integrator)(x, T::Real, mon::Monitor) = _propagate!(I.scheme, I.g, I.A, T, I.Δt, x, mon)
 
 # Integrator augmented with a quadrature function are callable with an additional argument.
-(I::Integrator{<:AugmentedSystem})(x, q, T::Real)               = I(aug_state(x, q), T)
-(I::Integrator{<:AugmentedSystem})(x, q, T::Real, mon::Monitor) = I(aug_state(x, q), T, mon)
+(I::Integrator{<:AugmentedSystem})(x, q, T::Real)               = 
+    _propagate!(I.scheme, I.g, I.A, T, I.Δt, aug_state(x, q), nothing)
+(I::Integrator{<:AugmentedSystem})(x, q, T::Real, mon::Monitor) = 
+    _propagate!(I.scheme, I.g, I.A, T, I.Δt, aug_state(x, q), mon)
 
 # Main propagation function
 @inline function _propagate!(scheme::IMEXRKScheme{Tab, S}, 
