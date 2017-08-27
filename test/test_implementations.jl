80c14b5de211fd9e424df300973512b87143d6bb
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-08-27 22:35:22 +0100

    fixed test

--- test/test_implementations.jl
+++ test/test_implementations.jl
@@ -1,6 +1,7 @@
 @testset "isembedded                             " begin
     for embed in [true, false]
-        for impl in [IMEXRK3R2R(IMEXRKCB3e, embed), 
+        for impl in [IMEXRK3R2R(IMEXRKCB2, embed), 
+                     IMEXRK3R2R(IMEXRKCB3e, embed), 
                      IMEXRK3R2R(IMEXRKCB3c, embed),
                      IMEXRK4R3R(IMEXRKCB4,  embed)]
             @test isembedded(impl)         == embed
@@ -10,15 +11,19 @@
 end
 
 @testset "nstorage                               " begin
+    @test nstorage(IMEXRK3R2R(IMEXRKCB2,  false)) == 3
     @test nstorage(IMEXRK3R2R(IMEXRKCB3e, false)) == 3
     @test nstorage(IMEXRK3R2R(IMEXRKCB3c, false)) == 3
     @test nstorage(IMEXRK4R3R(IMEXRKCB4,  false)) == 4
+    @test nstorage(IMEXRK3R2R(IMEXRKCB2,  true))  == 4
     @test nstorage(IMEXRK3R2R(IMEXRKCB3e, true))  == 4
     @test nstorage(IMEXRK3R2R(IMEXRKCB3c, true))  == 4
     @test nstorage(IMEXRK4R3R(IMEXRKCB4,  true))  == 5
+    @test nstorage(typeof(IMEXRK3R2R(IMEXRKCB2,  false))) == 3
     @test nstorage(typeof(IMEXRK3R2R(IMEXRKCB3e, false))) == 3
     @test nstorage(typeof(IMEXRK3R2R(IMEXRKCB3c, false))) == 3
     @test nstorage(typeof(IMEXRK4R3R(IMEXRKCB4,  false))) == 4
+    @test nstorage(typeof(IMEXRK3R2R(IMEXRKCB2,  true)))  == 4
     @test nstorage(typeof(IMEXRK3R2R(IMEXRKCB3e, true)))  == 4
     @test nstorage(typeof(IMEXRK3R2R(IMEXRKCB3c, true)))  == 4
     @test nstorage(typeof(IMEXRK4R3R(IMEXRKCB4,  true)))  == 5
