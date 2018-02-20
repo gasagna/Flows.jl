7c2806fa5e67e9f96e48e83c885af814f2c085e4
Davide Lasagna <davide.lasagna@soton.ac.uk>
2018-02-20 18:53:42 +0000

    tested steps

--- src/steps.jl
+++ src/steps.jl
@@ -1,32 +1,37 @@
 export IMEXMethod
 
-struct IMEXMethod{X, TAB<:AbstractTableau, I}
+# The third parameter is a symbol defining the implementation of the method,
+# e.g. :3R2R for the 3 register implementation of 2R methods.
+struct IMEXMethod{X, T<:Real, I}
     storage::Vector{X}
-    tableau::TAB
-    function IMEXMethod{I}(tableau::TAB, nstores::Int, x::X) where {X, TAB<:AbstractTableau}
-        new{X, TAB, I}(X[similar(x) for i = 1:nstores], tableau)
+    tableau::IMEXTableau{T}
+    function IMEXMethod{X, T, I}(tab::IMEXTableau, nstores::Int, x::X) where {X, T, I}
+        new(X[similar(x) for i = 1:nstores], convert(IMEXTableau{T}, tab))
     end
 end
 
+IMEXMethod(impl::Symbol, tab::IMEXTableau, nstores::Int, x::X) where {X} =
+    IMEXMethod{X, eltype(x), impl}(tab, nstores, x)
+
 # allowed method tags
-const __allowed__ = Dict{Symbol, Tuple{IMEXTableau, Int, Symbol}}(:CB2_3R2R  => (CB2,  3, :3R2R), 
-                                                                  :CB3c_3R2R => (CB3c, 3, :3R2R), 
-                                                                  :CB3e_3R2R => (CB3e, 3, :3R2R),
-                                                                  :CB4_4R3R  => (CB4,  4, :4R3R))
+const __allowed__ = Dict{Symbol, Tuple{IMEXTableau, Int, Symbol}}(:CB2_3R2R  => (CB2,  3, :_3R2R),
+                                                                  :CB3c_3R2R => (CB3c, 3, :_3R2R),
+                                                                  :CB3e_3R2R => (CB3e, 3, :_3R2R),
+                                                                  :CB4_4R3R  => (CB4,  4, :_4R3R))
 
 # Main entry point
-function IMEXMethod(tag::Symbol, x::X, q...)
+function IMEXMethod(tag::Symbol, x::X, q...) where {X}
     tag ∈ keys(__allowed__) || error("invalid method tag $tag")
-    tableau, nstores, impl = __allowed__[tag]
-    IMEXMethod{impl}(tableau, nstores, aug_state(x, q...))
+    tab, nstores, impl = __allowed__[tag]
+    IMEXMethod(impl, tab, nstores, aug_state(x, q...))
 end
 
 # Three-register implementation of [2R] IMEXRK schemes from section 1.2.1 of CB 2015
-function step!(method::IMEXMethod{X, TAB, :3R2R}, 
-               sys::System, 
-               t::Real, 
-               Δt::Real, 
-               x::X) where {X, TAB}
+function step!(method::IMEXMethod{X, T, :_3R2R},
+               sys::System,
+               t::Real,
+               Δt::Real,
+               x::X) where {X, T}
 
     # hoist temporaries out
     y   = method.storage[1]
@@ -39,8 +44,8 @@ function step!(method::IMEXMethod{X, TAB, :3R2R},
         if k == 1
             y .= x
         else
-            y .= x .+ (tab[:aᴵ, k, k-1] - tab[:bᴵ, k-1])*Δt.*z 
-                   .+ (tab[:aᴱ, k, k-1] - tab[:bᴱ, k-1])*Δt.*y
+            y .= x .+ (tab[:aᴵ, k, k-1] - tab[:bᴵ, k-1])*Δt.*z .+
+                      (tab[:aᴱ, k, k-1] - tab[:bᴱ, k-1])*Δt.*y
         end
         A_mul_B!(z, sys, y)                 # compute z = A*y then
         ImcA!(sys, tab[:aᴵ, k, k]*Δt, z, z) # get z = (I-cA)⁻¹*(A*y) in place
@@ -53,17 +58,17 @@ end
 
 
 # Four-register implementation of [3R] IMEXRK schemes from section 1.2.3 of CB 2015
-function step!(method::IMEXMethod{X, TAB, :4R3R}, 
-               sys::System, 
-               t::Real, 
-               Δt::Real, 
-               x::X) where {X, TAB}
+function step!(method::IMEXMethod{X, T, :_4R3R},
+               sys::System,
+               t::Real,
+               Δt::Real,
+               x::X) where {X, T}
 
     # hoist temporaries out
-    y   = method.store[1]
-    zᴵ  = method.store[2]
-    zᴱ  = method.store[3]
-    w   = method.store[4]
+    y   = method.storage[1]
+    zᴵ  = method.storage[2]
+    zᴱ  = method.storage[3]
+    w   = method.storage[4]
     tab = method.tableau
 
     # loop over stages
@@ -75,8 +80,8 @@ function step!(method::IMEXMethod{X, TAB, :4R3R},
         else
             zᴱ .= y .+ tab[:aᴱ, k, k-1]*Δt.*zᴱ
             if k < nstages(tab)
-                y .= x .+ (tab[:aᴵ, k+1, k-1] .- tab[:bᴵ, k-1]).*Δt.*zᴵ 
-                       .+ (tab[:aᴱ, k+1, k-1] .- tab[:bᴱ, k-1])./tab[:aᴱ, k+1, k-1].*(zᴱ .- y)
+                y .= x .+ (tab[:aᴵ, k+1, k-1] .- tab[:bᴵ, k-1]).*Δt.*zᴵ .+
+                          (tab[:aᴱ, k+1, k-1] .- tab[:bᴱ, k-1])./tab[:aᴱ, k, k-1].*(zᴱ .- y)
             end
             zᴱ .= zᴱ .+ tab[:aᴵ, k, k-1]*Δt.*zᴵ
         end
@@ -84,7 +89,7 @@ function step!(method::IMEXMethod{X, TAB, :4R3R},
         ImcA!(sys, tab[:aᴵ, k, k]*Δt, w, zᴵ) # compute z = (I-cA)⁻¹*w in place
         w .= zᴱ .+ tab[:aᴵ, k, k]*Δt.*zᴵ     # w is the temp input, output is zᴱ
         sys(t + tab[:cᴱ, k]*Δt, w, zᴱ)
-        x .= x .+ tab[:bᴵ, k]*Δt.*zᴵ .+ tab[:bᴱ, k]*Δt.*zᴱ        
+        x .= x .+ tab[:bᴵ, k]*Δt.*zᴵ .+ tab[:bᴱ, k]*Δt.*zᴱ
     end
     return nothing
 end
