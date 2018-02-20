53a7d9b98d6236b8ba2da781f4d188474373f400
Davide Lasagna <davide.lasagna@soton.ac.uk>
2018-02-20 21:41:22 +0000

    renamed variable

--- src/integrator.jl
+++ src/integrator.jl
@@ -1,32 +1,32 @@
 export integrator
 
-struct Integrator{S<:System, Sc}
+struct Integrator{S<:System, M<:IMEXMethod}
       system::S       # the system to be integrated
-      scheme::Sc      # the scheme, with storage and RK implementation
+      method::M      # the method, with storage and RK implementation
           Δt::Float64 # the time step. Will be replaced by integration options
     function Integrator(system::S,
-                        scheme::Sc,
-                        Δt::Real) where {S, Sc}
+                        method::M,
+                        Δt::Real) where {S, M}
         Δt > 0 || throw(ArgumentError("Δt must be positive, got $Δt"))
-        new{S, Sc}(system, scheme, Δt)
+        new{S, M}(system, method, Δt)
     end
 end
 
 # Outer constructor
-integrator(g, A, q, scheme::IMEXMethod, Δt::Real) = Integrator(System(g, A,             q), scheme, Δt)
-integrator(g, A,    scheme::IMEXMethod, Δt::Real) = Integrator(System(g, A,       nothing), scheme, Δt)
-integrator(g,       scheme::IMEXMethod, Δt::Real) = Integrator(System(g, nothing, nothing), scheme, Δt)
+integrator(g, A, q, method::IMEXMethod, Δt::Real) = Integrator(System(g, A,             q), method, Δt)
+integrator(g, A,    method::IMEXMethod, Δt::Real) = Integrator(System(g, A,       nothing), method, Δt)
+integrator(g,       method::IMEXMethod, Δt::Real) = Integrator(System(g, nothing, nothing), method, Δt)
 
 # Main entry points. Integrators are callable objects ...
 @inline (I::Integrator)(x, span::NTuple{2, Real}, mon::Vararg{Monitor}) =
-    _propagate!(I.scheme, I.system, span, I.Δt, x, mon...)
+    _propagate!(I.method, I.system, span, I.Δt, x, mon...)
 
 # Integrator augmented with a quadrature function are callable with an additional argument.
 @inline (I::Integrator)(x, q, span::NTuple{2, Real}, mon::Vararg{Monitor}) =
-    _propagate!(I.scheme, I.system, span, I.Δt, aug_state(x, q), mon...)
+    _propagate!(I.method, I.system, span, I.Δt, aug_state(x, q), mon...)
 
 # Main propagation function
-@generated function _propagate!(scheme::IMEXMethod{S},
+@generated function _propagate!(method::IMEXMethod{S},
                                 system::System,
                                 span::NTuple{2, Real},
                                 Δt::Float64, # enforced to be positive
@@ -45,7 +45,7 @@ integrator(g,       scheme::IMEXMethod, Δt::Real) = Integrator(System(g, nothin
         for j = 2:length(ts)
 
             # advance
-            step!(scheme, system, ts[j-1], ts[j]-ts[j-1], z)
+            step!(method, system, ts[j-1], ts[j]-ts[j-1], z)
 
             # store solution into monitor
             Base.Cartesian.@nexprs $N i->push!(mon[i], ts[j], _state(z))
