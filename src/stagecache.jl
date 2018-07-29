654f28b1aba60a921fba64645b466d2009590905
Davide Lasagna <lasagnadavide@gmail.com>
2018-07-29 19:06:48 +0100

    defined stagecache type

---
+++ src/stagecache.jl
@@ -0,0 +1,25 @@
+export RAMStageCache
+
+# ---------------------------------------------------------------------------- #
+# Abstract type for all stage caches. The first parameters is the number
+# of cached intermediate stage values, and the second is their type, e.g.
+# some subtype of an AbstractArray.
+abstract type AbstractStageCache{N, X} end
+
+# ---------------------------------------------------------------------------- #
+# Do not cache the stages
+struct NoCache{N, X} <: AbstractStageCache{N, X} end
+
+# ---------------------------------------------------------------------------- #
+# Stage cache where all stages are cached in RAM.
+struct RAMStageCache{N, X} <: AbstractStageCache{N, X}
+     ts::Vector{Float64}
+    Δts::Vector{Float64}
+     xs::Vector{NTuple{N, X}}
+end
+
+@inline Base.push!(ss::RAMStageCache{N, X},
+                    t::Real,
+                   Δt::Real,
+                    x::NTuple{N, X}) where {N, X} =
+    (push!(ss.ts, t); push!(ss.Δts, Δt); push!(ss.xs, x); nothing)
