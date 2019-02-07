f2c76d9423f86bf39adf052f96f410e9054e2a63
Davide Lasagna <lasagnadavide@gmail.com>
2019-02-07 22:16:59 +0000

    restore adjoint code for CB3r2r

--- src/steps/CB3R2R.jl
+++ src/steps/CB3R2R.jl
@@ -9,14 +9,14 @@ for (name, tab) in zip((:CB3R2R2, :CB3R2R3e, :CB3R2R3c), (CB2, CB3e, CB3c))
 
 # type
 struct $name{X, NS, TAG, ISADJ} <: AbstractMethod{X, NS, TAG, ISADJ}
-    store::NTuple{3, X}
+    store::NTuple{5, X}
 end
 
 # outer constructor
 $name(x::X, tag::Symbol) where {X} =
-    $name{X, $(nstages(tab)), _tag_map(tag)...}(ntuple(i->similar(x), 3))
+    $name{X, $(nstages(tab)), _tag_map(tag)...}(ntuple(i->similar(x), 5))
 
-# required to cope with nuggy julia deepcopy implementation
+# required to cope with buggy julia deepcopy implementation
 function Base.deepcopy_internal(x::$name, dict::IdDict)
     if !( haskey(dict, x) )
         dict[x] = $name(x.store[1], typetag(x))
@@ -34,7 +34,7 @@ function step!(method::$name{X, NS, :NORMAL},
                     c::C) where {X, NS, C<:Union{Nothing, AbstractStageCache{NS, X}}}
 
     # hoist temporaries out
-    y, z, w  = method.store
+    y, z, w, s, v  = method.store
     tab = $tab
 
     # temporary vector for storing stages
@@ -73,7 +73,7 @@ function step!(method::$name{X, NS, :LIN, false},
                     x::X,
                stages::NTuple{NS, X}) where {X, NS}
     # hoist temporaries out
-    y, z, w  = method.store
+    y, z, w, v, s  = method.store
     tab = $tab
 
     # loop over stages
@@ -112,35 +112,75 @@ function step!(method::$name{X, NS, :LIN, true},
                    Δt::Real,
                     x::X,
                stages::NTuple{NS, X}) where {X, NS}
-    # hoist temporaries out
-    y, z, w  = method.store
-    @all y .= 0; @all z .= 0; @all w .= 0;
-    tab = $tab
 
-    # loop over stages backwards
+    # hoist temporaries out
+    y, z, w, v, s  = method.store
+    y .= 0; z .= 0; w .= 0; v .= 0; s .= 0;
+    tab = $tab
+    
     @inbounds for k = reverse(1:NS)
         # A
-        @all z .= z .+ tab[:bᴵ, k].*Δt.*x
-        @all y .= y .+ tab[:bᴱ, k].*Δt.*x
+        z .= z .+ tab[:bᴵ, k].*Δt.*x
+        y .= y .+ tab[:bᴱ, k].*Δt.*x
         # B
-        # We will probably have to think to another way to define the forcings 
-        # for linearised system that require the time derivative of the main state.
-        sys(t + tab[:cᴱ, k]*Δt, stages[k], y, w)
+        v .= w # temporary
+        sys(t + tab[:cᴱ, k]*Δt, stages[k], y, v)
+        w .= w .+ v
+        y .= 0
         # C
-        @all z .= z .+ tab[:aᴵ, k, k].*Δt.*w
-        @all y .= w
+        z .= z .+ tab[:aᴵ, k, k].*Δt.*w
+        y .= y .+ w
+        w .= 0
         # D
-        ImcA!(sys, tab[:aᴵ, k, k]*Δt, z, w)
+        ImcA!(sys, tab[:aᴵ, k, k]*Δt, z, v)
+        s .= v .+ s
+        z .= 0
         # E
-        mul!(z, sys, w)
-        @all y .= z .+ y
+        mul!(v, sys, s)
+        y .= v .+ y
+        s .= 0
         # F
-        @all x .= x .+ y
-        if k > 1
-            @all z .= (tab[:aᴵ, k, k-1] .- tab[:bᴵ, k-1]).*Δt.*y
-            @all y .= (tab[:aᴱ, k, k-1] .- tab[:bᴱ, k-1]).*Δt.*y
+        if k == 1
+            x .= x .+ y
+            y .= 0
+        else
+            x .= y .+ x
+            z .= (tab[:aᴵ, k, k-1] .- tab[:bᴵ, k-1]).*Δt.*y .+ z
+            y .= (tab[:aᴱ, k, k-1] .- tab[:bᴱ, k-1]).*Δt.*y
         end
     end
+
+
+    # this was not correct
+    # # hoist temporaries out
+    # y, z, w  = method.store
+    # @all y .= 0; @all z .= 0; @all w .= 0;
+    # tab = $tab
+
+    # # loop over stages backwards
+    # @inbounds for k = reverse(1:NS)
+    #     # A
+    #     @all z .= z .+ tab[:bᴵ, k].*Δt.*x
+    #     @all y .= y .+ tab[:bᴱ, k].*Δt.*x
+    #     # B
+    #     # We will probably have to think to another way to define the forcings 
+    #     # for linearised system that require the time derivative of the main state.
+    #     sys(t + tab[:cᴱ, k]*Δt, stages[k], y, w)
+    #     # C
+    #     @all z .= z .+ tab[:aᴵ, k, k].*Δt.*w
+    #     @all y .= w
+    #     # D
+    #     ImcA!(sys, tab[:aᴵ, k, k]*Δt, z, w)
+    #     # E
+    #     mul!(z, sys, w)
+    #     @all y .= z .+ y
+    #     # F
+    #     @all x .= x .+ y
+    #     if k > 1
+    #         @all z .= (tab[:aᴵ, k, k-1] .- tab[:bᴵ, k-1]).*Δt.*y
+    #         @all y .= (tab[:aᴱ, k, k-1] .- tab[:bᴱ, k-1]).*Δt.*y
+    #     end
+    # end
 
     return nothing
 end
